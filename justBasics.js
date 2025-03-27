fe50b00a22f99058041d1f2ae544f7dca175de78
Nandu Rajak <golanukondanandu@gmail.com>
2025-03-27 09:15:32 +0530

    small function added

--- justBasics.js
+++ justBasics.js
@@ -1,2 +1,13 @@
-let name
+let name = "zoro Bhai";
+function zoroAttacks() {
+  var attacks = ["snathoruyu.. Onigiri..!", "Dragon twist", "Tiger stirke"];
+  console.log(attacks);
+}
+function zoroNickNames() {
+  var nicknames = ["moss Head", "Roronoa", "king of hell"];
+  console.log(nicknames);
+}
+
 console.log(name);
+zoroAttacks();
+zoroNickNames();
