049288bf8db575dfd8b699c7131a3219cd38f997
Nandu Rajak <golanukondanandu@gmail.com>
2025-03-29 09:21:44 +0530

    slice, splice, shift, unshift, push and pop

--- arrays.js
+++ arrays.js
@@ -1,16 +1,16 @@
 // print array of numbers in decreasing order....
-// let array = [0, 1, 2, 3, 4, 5];
+let arrays = [0, 1, 2, 3, 4, 5];
 // console.log("A ", array.slice(0, 1));
 // console.log("B ", array.slice(0, 2));
 // console.log("C ", array.slice(0, 3));
 // console.log("D ", array.slice(0, 4));
 // console.log("E ", array.slice(0, 5));
 // console.log("F ", array.slice(0, 6));
-// let array = [0, 1, 2, 3, 4, 5];
-// let letters = ["A", "B", "C", "D", "E", "F"];
-// for (let i = 1; i <= array.length; i++) {
-//   console.log(letters[i - 1], array.slice(0, i));
-// }
+let array = [0, 1, 2, 3, 4, 5];
+for (let i = 1; i <= array.length; i++) {
+  //   console.log(letters[i - 1], array.slice(0, i));
+  let letters = ["A", "B", "C", "D", "E", "F"];
+}
 let itemsPrices = [299, 149, 599, 800, 1000, 400];
 for (let i = 0; i < itemsPrices.length; i++) {
   let discount = 25;
@@ -19,3 +19,22 @@ for (let i = 0; i < itemsPrices.length; i++) {
   //     `Final price of items after applying discount is : ${finalPrice}`
   //   );
 }
+
+// Using slice, splice, shift, unshift, push, pop
+let foodItems = [
+  "Biryani",
+  "Mutton Biryani",
+  "Paneer Tika Masala",
+  "Curd Rice",
+];
+const favFood = foodItems.slice(0, 2);
+// console.log(favFood);
+
+const deleteFoodItem = foodItems.shift();
+console.log(`Deleted FoodItem : ${deleteFoodItem}`);
+
+foodItems.push("Butter nan");
+console.log(foodItems);
+
+foodItems.unshift("Chicken Biryani");
+console.log(foodItems);
