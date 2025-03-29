eef3428d59d63e5a4bc6e72deeb940056086400a
Nandu Rajak <golanukondanandu@gmail.com>
2025-03-29 08:51:26 +0530

    used for loop in arrays

--- arrays.js
+++ arrays.js
@@ -6,8 +6,16 @@
 // console.log("D ", array.slice(0, 4));
 // console.log("E ", array.slice(0, 5));
 // console.log("F ", array.slice(0, 6));
-let array = [0, 1, 2, 3, 4, 5];
-let letters = ["A", "B", "C", "D", "E", "F"];
-for (let i = 1; i <= array.length; i++) {
-  console.log(letters[i - 1], array.slice(0, i));
+// let array = [0, 1, 2, 3, 4, 5];
+// let letters = ["A", "B", "C", "D", "E", "F"];
+// for (let i = 1; i <= array.length; i++) {
+//   console.log(letters[i - 1], array.slice(0, i));
+// }
+let itemsPrices = [299, 149, 599, 800, 1000, 400];
+for (let i = 0; i < itemsPrices.length; i++) {
+  let discount = 25;
+  let finalPrice = itemsPrices[i] - discount;
+  //   console.log(
+  //     `Final price of items after applying discount is : ${finalPrice}`
+  //   );
 }
