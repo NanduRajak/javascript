e0dd3ceb4c5160efbedaa521847361f17a5710c2
Nandu Rajak <golanukondanandu@gmail.com>
2025-04-10 20:06:49 +0530

    added HOFs

--- hOFs.js
+++ hOFs.js
@@ -1,3 +1,23 @@
 // map(),filter() and reduce() array methods.
-const array = [2, 45, 666, 798, 534];
-array.map(() => {});
+
+// map()
+const array = [2, 5, 6, 8, 4];
+const mappingAnta = array.map((value) => {
+  return value * 2;
+});
+// console.log(mappingAnta);
+
+// filter()
+
+const filteringAnta = array.filter((value) => {
+  return value % 2 == 0;
+});
+// console.log(filteringAnta);
+
+// reduce()
+
+const reduceAnta = array.reduce((acc, curr) => {
+  const val = acc * curr;
+  return val;
+});
+// console.log(reduceAnta);
