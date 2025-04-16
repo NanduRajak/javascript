80a5c83e95cd5ad60961fea1ca1436aa73552d49
Nandu Rajak <golanukondanandu@gmail.com>
2025-04-16 12:32:59 +0530

    promise.allSettled()

--- promises_APIs.js
+++ promises_APIs.js
@@ -1,26 +1,55 @@
-const P1 = new Promise((resolve, reject) => {
+// const P1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("P1 Success!");
+//   }, 2000);
+// });
+
+// const P2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("P2 Success!");
+//   }, 3000);
+// });
+
+// const P3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("P3 successful!");
+//     // reject("P3 Reajected!");
+//   }, 4000);
+// });
+
+// Promise.all([P1, P2, P3])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+// Promise.allSetteled()
+
+const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("P1 Success!");
   }, 2000);
 });
 
-const P2 = new Promise((resolve, reject) => {
+const p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("P2 Success!");
   }, 3000);
 });
 
-const P3 = new Promise((resolve, reject) => {
+const p3 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("P3 successful!");
-    // reject("P3 Reajected!");
+    // reject("P3 Rejected!");
   }, 4000);
 });
 
-Promise.all([P1, P2, P3])
-  .then((res) => {
-    console.log(res);
+Promise.allSettled([p1, p2, p3])
+  .then((result) => {
+    console.log(result);
   })
-  .catch((err) => {
-    console.error(err);
+  .catch((error) => {
+    console.error(error);
   });
