268878b7e0f75f07612fb2f661a10754b5b1b6fe
Nandu Rajak <golanukondanandu@gmail.com>
2025-04-18 13:23:33 +0530

    arrow function

--- thisKeyword.js
+++ thisKeyword.js
@@ -1,8 +1,14 @@
 const obj = {
   a: 10,
   x: function () {
-    console.log(this);
+    // console.log(this.a);
+    const y = () => {
+      console.log(this);
+    };
+    y();
   },
 };
-
 obj.x();
+
+// Global space
+// console.log(this);
